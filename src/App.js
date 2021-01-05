0c4023082fb9a8b17d86247d05389e98302d8a57
The Mindful Creator <themindfulcreator@gmail.com>
2021-01-05 23:38:47 +0800

    Initial update for app.js
    Created new repository for I'VALEAU website

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          The official website of I'VALEAU.
         </a>
       </header>
     </div>
